04514a20cbe5374cf6537ddb074eac9315826dd1
vergincoder <bulatmotors@gmail.com>
2022-12-26 14:56:01 +0300

    FirstCommit

--- library/firstapp/models.py
+++ library/firstapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    book_name = models.CharField('book_name', max_length=50)
+    book_author = models.CharField('book_author', max_length=50)
+    date_of_create = models.DateField('date_of_create')
+    category = models.ManyToManyField(Category)
