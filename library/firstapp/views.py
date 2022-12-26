6bd361fab6877e867501d2243923c402a6e33d23
pishukommentu-DanllI <pishukommentu-DanllI>
2022-12-26 13:48:36 +0300

    'form'

--- library/firstapp/views.py
+++ library/firstapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+
+class Index(ListView):
+    model = 'Model'
+    template_name = ''
+    context_object_name = ''
+
+
+    def get_queryset(self):
+        pass
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Main'
+
